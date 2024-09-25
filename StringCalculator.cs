2da08cb52bbd2c8be26f8e06279c2851ec8e8780
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-09-25 17:29:40 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -1,1 +1,51 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
+public class StringCalculator
+{
+    public static int StringSumCalculator(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return 0;
+
+        //validates 
+        ValidateCharacterAfterNewline(input);
+		int result  = CalculateSum(input);
+        return result;
+    }
+
+    private static void ValidateCharacterAfterNewline(string input)
+    {
+        if (Regex.IsMatch(input, @"\n[^\d\w]")
+        {
+            throw new InvalidOperationException("Not valid input: not allowed to add delimter after \"\n\"");
+        }
+    }
+
+    private static int CalculateSum(string input)
+    {
+        // Regular expression that removes excess negative signs
+        string preprocessedNumbers = Regex.Replace(input, @"-+", "-", RegexOptions.None);
+        // Regular expression that gets numbers separated by delimiters
+        MatchCollection NumbersCollection = Regex.Matches(preprocessedNumbers, @"\b-?\d+\b");
+        int sum = 0;
+        foreach (Match match in NumbersCollection)
+        {
+            int number = int.Parse(match.Value); //parse the matched value
+
+            sum += ValidateAndSum(number); // Validates number and sum the number
+        }
+        return sum;
+    }
+    private static int ValidateAndSum(int number)
+    {
+        if (number < 0) //throws exception when number is negative
+        {
+            throw new InvalidOperationException("No Negative Numbers are Allowed!");
+        }
+
+        return number < 1000 ? number : 0;
+    }
+
+}
