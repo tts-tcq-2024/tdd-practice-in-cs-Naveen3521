ae44bde0edb3a8a4b1a8c761a4938df05db238bd
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-09-25 18:13:04 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -2,33 +2,34 @@ using System;
 using System.Text;
 using System.Text.RegularExpressions;
 
+
 public class StringCalculator
 {
-    public static int StringSumCalculator(string input)
+    public int StringSumCalculator(string input)
     {
         if (string.IsNullOrEmpty(input))
             return 0;
 
-        //validates 
+        
         ValidateCharacterAfterNewline(input);
-		int result  = CalculateSum(input);
+	int result  = CalculateSum(input);
         return result;
     }
 
-    private static void ValidateCharacterAfterNewline(string input)
+    private void ValidateCharacterAfterNewline(string input)
     {
-        if (Regex.IsMatch(input, @"\n[^\d\w]")
+        if (Regex.IsMatch(input, @"\n[^\d\w]"))
         {
-            throw new InvalidOperationException("Not valid input: not allowed to add delimter after \"\n\"");
+            throw new System.Exception("Not valid input: no special character allowed after delimter \"\n\"");
         }
     }
 
-    private static int CalculateSum(string input)
+    private int CalculateSum(string input)
     {
         // Regular expression that removes excess negative signs
         string preprocessedNumbers = Regex.Replace(input, @"-+", "-", RegexOptions.None);
         // Regular expression that gets numbers separated by delimiters
-        MatchCollection NumbersCollection = Regex.Matches(preprocessedNumbers, @"-?\d+");
+        MatchCollection NumbersCollection = Regex.Matches(preprocessedNumbers, @"\b-?\d+\b");
         int sum = 0;
         foreach (Match match in NumbersCollection)
         {
@@ -38,11 +39,11 @@ public class StringCalculator
         }
         return sum;
     }
-    private static int ValidateAndSum(int number)
+    private int ValidateAndSum(int number)
     {
         if (number < 0) //throws exception when number is negative
         {
-            throw new InvalidOperationException("No Negative Numbers are Allowed!");
+            throw new System.Exception("No Negative Numbers are Allowed!");
         }
 
         return number < 1000 ? number : 0;
