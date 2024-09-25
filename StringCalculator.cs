abe2d6d08ff150348de22e037ee670137eb4c2ed
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-09-25 18:08:49 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -1,74 +1,51 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
-
-namespace StringCalculatorApp
+public class StringCalculator
 {
-    public class Program
+    public static int StringSumCalculator(string input)
     {
+        if (string.IsNullOrEmpty(input))
+            return 0;
 
-        public class HelloWorld
+        //validates 
+        ValidateCharacterAfterNewline(input);
+		int result  = CalculateSum(input);
+        return result;
+    }
+
+    private static void ValidateCharacterAfterNewline(string input)
+    {
+        if (Regex.IsMatch(input, @"\n[^\d\w]")
         {
-            public static void Main(string[] args)
-            {
-                StringCalculator cal = new StringCalculator();
-                int result = cal.StringSumCalculator("-1.2");
-                Console.WriteLine(result);
-            }
-        }
-
-
-
-        public class StringCalculator
-        {
-            public int StringSumCalculator(string input)
-            {
-                if (string.IsNullOrEmpty(input))
-                    return 0;
-
-                //validates 
-                ValidateCharacterAfterNewline(input);
-                int result = CalculateSum(input);
-                return result;
-            }
-
-            private void ValidateCharacterAfterNewline(string input)
-            {
-                if (Regex.IsMatch(input, @"\n[^\d\w]"))
-                {
-                    throw new System.Exception("Not valid input: not allowed to add delimter after \"\n\"");
-                }
-            }
-
-            private int CalculateSum(string input)
-            {
-                // Regular expression that removes excess negative signs
-                string preprocessedNumbers = Regex.Replace(input, @"-+", "-", RegexOptions.None);
-                // Regular expression that gets numbers separated by delimiters
-                MatchCollection NumbersCollection = Regex.Matches(preprocessedNumbers, @"-?\d+");
-                int sum = 0;
-                foreach (Match match in NumbersCollection)
-                {
-                    int number = int.Parse(match.Value); //parse the matched value
-
-                    sum += ValidateAndSum(number); // Validates number and sum the number
-                }
-                return sum;
-            }
-            private int ValidateAndSum(int number)
-            {
-                if (number < 0) //throws exception when number is negative
-                {
-                    throw new System.Exception("No Negative Numbers are Allowed!");
-                }
-
-                return number < 1000 ? number : 0;
-            }
-
+            throw new InvalidOperationException("Not valid input: not allowed to add delimter after \"\n\"");
         }
     }
+
+    private static int CalculateSum(string input)
+    {
+        // Regular expression that removes excess negative signs
+        string preprocessedNumbers = Regex.Replace(input, @"-+", "-", RegexOptions.None);
+        // Regular expression that gets numbers separated by delimiters
+        MatchCollection NumbersCollection = Regex.Matches(preprocessedNumbers, @"-?\d+");
+        int sum = 0;
+        foreach (Match match in NumbersCollection)
+        {
+            int number = int.Parse(match.Value); //parse the matched value
+
+            sum += ValidateAndSum(number); // Validates number and sum the number
+        }
+        return sum;
+    }
+    private static int ValidateAndSum(int number)
+    {
+        if (number < 0) //throws exception when number is negative
+        {
+            throw new InvalidOperationException("No Negative Numbers are Allowed!");
+        }
+
+        return number < 1000 ? number : 0;
+    }
+
 }
